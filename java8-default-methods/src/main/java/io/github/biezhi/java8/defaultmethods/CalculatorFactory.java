e6f8b7d2418c2b91be26d7638c1f344aa7e367a5
biezhi <biezhi.me@gmail.com>
2018-02-12 01:06:52 +0800

    :bento: update default method video url

--- java8-default-methods/src/main/java/io/github/biezhi/java8/defaultmethods/CalculatorFactory.java
+++ java8-default-methods/src/main/java/io/github/biezhi/java8/defaultmethods/CalculatorFactory.java
@@ -4,7 +4,7 @@ package io.github.biezhi.java8.defaultmethods;
  * @author biezhi
  * @date 2018/2/11
  */
-public class CalculatorFactoy {
+public class CalculatorFactory {
 
     public static Calculator getInstance(){
         return new BasicCalculator();
