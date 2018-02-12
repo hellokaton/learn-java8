fb75d28fcbc9fb9106fa95bf3d3d380cd31729b6
biezhi <biezhi.me@gmail.com>
2018-02-12 15:00:13 +0800

    :bento: add stream2 end homework

---
+++ java8-stream/src/main/java/io/github/biezhi/java8/stream/lesson2/QuizEnd.java
@@ -0,0 +1,62 @@
+package io.github.biezhi.java8.stream.lesson2;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * (1) 找出2011年发生的所有交易，并按交易额排序（从低到高）。
+ * (2) 交易员都在哪些不同的城市工作过？
+ * (3) 查找所有来自于剑桥的交易员，并按姓名排序。
+ * (4) 返回所有交易员的姓名字符串，按字母顺序排序。
+ * (5) 有没有交易员是在米兰工作的？
+ * (6) 打印生活在剑桥的交易员的所有交易额。
+ * (7) 所有交易中，最高的交易额是多少？
+ * (8) 找到交易额最小的交易。
+ *
+ * @author biezhi
+ * @date 2018/2/12
+ */
+public class QuizEnd {
+
+    // 交易员
+    @Data
+    @AllArgsConstructor
+    static class Trader {
+        // 姓名
+        private String name;
+        // 城市
+        private String city;
+    }
+
+    // 交易
+    @Data
+    @AllArgsConstructor
+    static class Transaction {
+        private Trader trader;
+        // 交易年份
+        private int    year;
+        // 交易额
+        private int    value;
+    }
+
+    public static void main(String[] args) {
+        Trader raoul = new Trader("Raoul", "Cambridge");
+        Trader mario = new Trader("Mario", "Milan");
+        Trader alan  = new Trader("Alan", "Cambridge");
+        Trader brian = new Trader("Brian", "Cambridge");
+
+        List<Transaction> transactions = Arrays.asList(
+                new Transaction(brian, 2011, 300),
+                new Transaction(raoul, 2012, 1000),
+                new Transaction(raoul, 2011, 400),
+                new Transaction(mario, 2012, 710),
+                new Transaction(mario, 2012, 700),
+                new Transaction(alan, 2012, 950)
+        );
+
+    }
+
+}
