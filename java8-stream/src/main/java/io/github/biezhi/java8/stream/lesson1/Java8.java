7eea9c2ab602e52d35a2dc8944b7b46574a37749
biezhi <biezhi.me@gmail.com>
2018-03-01 00:11:17 +0800

    :memo: update stream-1 course

--- java8-stream/src/main/java/io/github/biezhi/java8/stream/lesson1/Java8.java
+++ java8-stream/src/main/java/io/github/biezhi/java8/stream/lesson1/Java8.java
@@ -14,11 +14,19 @@ public class Java8 {
     public static void main(String[] args) {
         List<Project> projects = Project.buildData();
         List<String> names = projects.stream()
-                .filter(p -> p.getStars() > 1000)
-                .map(Project::getName)
+                .filter(p -> {
+                    System.out.println(p.getName());
+                    return p.getStars() > 1000;
+                })
+                .map(p -> {
+                    System.out.println(p.getName());
+                    return p.getName();
+                })
                 .limit(3)
                 .collect(Collectors.toList());
         System.out.println(names);
+
+        names.stream().forEach(name-> System.out.println(name));
     }
 
 }
