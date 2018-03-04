e96f9fdab0f5f84881062988ecf88b76b3c18fe0
biezhi <biezhi.me@gmail.com>
2018-03-04 13:26:49 +0800

    :memo: update stream example

--- java8-stream/src/main/java/io/github/biezhi/java8/stream/lesson3/Example6.java
+++ java8-stream/src/main/java/io/github/biezhi/java8/stream/lesson3/Example6.java
@@ -1,6 +1,10 @@
 package io.github.biezhi.java8.stream.lesson3;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 并行流
@@ -11,19 +15,19 @@ import java.util.Arrays;
 public class Example6 {
 
     public static void main(String[] args) {
-        Arrays.asList("a1", "a2", "b1", "c2", "c1")
-                .parallelStream()
-                .filter(s -> {
-                    System.out.format("filter: %s [%s]\n",
-                            s, Thread.currentThread().getName());
-                    return true;
-                })
-                .map(s -> {
-                    System.out.format("map: %s [%s]\n",
-                            s, Thread.currentThread().getName());
-                    return s.toUpperCase();
-                })
-                .forEach(s -> System.out.format("forEach: %s [%s]\n",
-                        s, Thread.currentThread().getName()));
+        int          max    = 1000000;
+        List<String> values = new ArrayList<>(max);
+        for (int i = 0; i < max; i++) {
+            UUID uuid = UUID.randomUUID();
+            values.add(uuid.toString());
+        }
+
+        long t0 = System.nanoTime();
+        long count = values.stream().sorted().count();
+        System.out.println(count);
+        long t1 = System.nanoTime();
+        long millis = TimeUnit.NANOSECONDS.toMillis(t1 - t0);
+        System.out.println(String.format("sequential sort took: %d ms", millis));
+
     }
 }
