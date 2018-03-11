aad683e8818d0a8877ad0b01636a437a39e79c2a
biezhi <biezhi.me@gmail.com>
2018-03-11 18:05:06 +0800

    :memo: add concurrent code example

---
+++ java8-concurrent/src/main/java/io/github/biezhi/java8/concurrent/ConcurrentUtils.java
@@ -0,0 +1,39 @@
+package io.github.biezhi.java8.concurrent;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 并发工具类
+ *
+ * @author biezhi
+ * @date 2018/3/11
+ */
+public class ConcurrentUtils {
+
+    public static void stop(ExecutorService executor) {
+        try {
+            executor.shutdown();
+            executor.awaitTermination(60, TimeUnit.SECONDS);
+        }
+        catch (InterruptedException e) {
+            System.err.println("termination interrupted");
+        }
+        finally {
+            if (!executor.isTerminated()) {
+                System.err.println("killing non-finished tasks");
+            }
+            executor.shutdownNow();
+        }
+    }
+
+    public static void sleep(int seconds) {
+        try {
+            TimeUnit.SECONDS.sleep(seconds);
+        } catch (InterruptedException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+
+}
